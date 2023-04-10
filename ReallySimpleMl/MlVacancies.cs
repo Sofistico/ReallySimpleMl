d9aca26a79d8abe454fee2a0a9695c182d0cb5db
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2023-04-10 19:09:20 -0300

    Begin of the beginnning

---
+++ ReallySimpleMl/MlVacancies.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Spectre.Console;
+using System.Linq;
+using Console = Spectre.Console.AnsiConsole;
+
+namespace ReallySimpleMl
+{
+    public class MlVacancies
+    {
+        private readonly MLContext ctx;
+        private readonly IDataView dataView;
+
+        public MlVacancies()
+        {
+            ctx = new();
+            dataView = ctx.Data.LoadFromTextFile<VacancyData>("vacancy.txt");
+        }
+
+        public ITransformer TrainMl()
+        {
+            // split data into testing set
+            var splitDataView = ctx.Data
+                .TrainTestSplit(dataView, testFraction: 0.2);
+
+            // build model
+            var estimator = ctx.Transforms.Text
+                .FeaturizeText(
+                    outputColumnName: "Features",
+                    inputColumnName: nameof(VacancyData.Text)
+                ).Append(ctx.BinaryClassification.Trainers.SdcaLogisticRegression(featureColumnName: "Features"));
+
+            // Train model
+            ITransformer model = default!;
+            var rule = new Spectre.Console.Rule("Create and Train Model");
+            Console
+                .Live(rule)
+                .Start(console =>
+                {
+                    // training happens here
+                    model = estimator.Fit(splitDataView.TrainSet);
+                    var predictions = model.Transform(splitDataView.TestSet);
+                    rule.Title = "🏁 Training Complete, Evaluating Accuracy.";
+                    console.Refresh();
+                    // evaluate the accuracy of our model
+                    var metrics = ctx.BinaryClassification.Evaluate(predictions);
+                    var table = new Table()
+                        .MinimalBorder()
+                        .Title("💯 Model Accuracy");
+                    table.AddColumns("Accuracy", "Auc", "F1Score");
+                    table.AddRow($"{metrics.Accuracy:P2}", $"{metrics.AreaUnderRocCurve:P2}", $"{metrics.F1Score:P2}");
+                    console.UpdateTarget(table);
+                    console.Refresh();
+                });
+            return model;
+        }
+
+        public void ExecuteMl(ITransformer model)
+        {
+            while (true)
+            {
+                var text = Console.Ask<string>("[red]Qual o seu [/][green]texto para revisão[/]?");
+                var engine = ctx.Model.CreatePredictionEngine<VacancyData, VacancyPrediction>(model);
+                var input = new VacancyData { Text = text };
+                var result = engine.Predict(input);
+                var (color, vacancy) = result.Prediction
+                    ? (color: "green", emoji: "É uma vaga")
+                    : (color: "red", emoji: "Não é uma vaga");
+                Console.MarkupLine($"{vacancy} [{color}]\"{text}\" ({result.Probability:P00})[/] ");
+            }
+        }
+    }
+
+    public class VacancyData
+    {
+        [LoadColumn(0)]
+        public string? Text { get; set; }
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Vacancy { get; set; }
+    }
+
+    public class VacancyPrediction : VacancyData
+    {
+        [ColumnName("PredictedLabel")] public bool Prediction { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
--- ReallySimpleMl/Program.cs
+++ ReallySimpleMl/Program.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReallySimpleMl
 {
     public class Program
     {
+        private static readonly Dictionary<int, string> keys = new Dictionary<int, string>()
+        {
+            {0, "Yelp!" },
+            {1, "Descobre se é vaga" },
+        };
+
         private static void Main(string[] args)
         {
-            Yelp yelp = new Yelp();
-            var model = yelp.TrainMl();
-            yelp.ExecuteMl(model);
+            Console.WriteLine("Selecione um modelo abaixo de ML:");
+            foreach (var key in keys)
+            {
+                Console.WriteLine($"{key.Key} - {key.Value}");
+            }
+            int value;
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Selecione um valor valido!");
+            }
+
+            switch (value)
+            {
+                case 0:
+                    Yelp yelp = new Yelp();
+                    var model = yelp.TrainMl();
+                    yelp.ExecuteMl(model);
+                    break;
+
+                case 1:
+                    MlVacancies mlVacancies = new MlVacancies();
+                    var vacancyModel = mlVacancies.TrainMl();
+                    mlVacancies.ExecuteMl(vacancyModel);
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
